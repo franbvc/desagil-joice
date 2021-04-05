4ab0f778d3667e99d96b336bf9598369b07bef4e
Luiza Valezim Augusto Pinto <62949391+LuizaValezim@users.noreply.github.com>
2021-04-05 16:30:06 -0300

    Calculadora CFC e Teste

--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -1,5 +1,19 @@
 package br.edu.insper.desagil.joice.model;
 
-public class CalculadoraCFC {
+public class CalculadoraCFC extends Calculadora{
+	
+	public CalculadoraCFC() {
+		super("CFC");
+	}
+	
+	@Override
+	public double calcula(double peso, double raio) {
+		double raioCm = raio * Math.pow(10, -7);
+		double Vc =  Math.pow((2*raioCm*Math.sqrt(2)), 3);
+		double Na = 6.023 * Math.pow(10, 23);
+		double n = 4;
+		return (double) ((n*peso)/(Na*Vc));
+	}
 
 }
+
--- test/br/edu/insper/desagil/joice/model/CalculadoraCFCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCFCTest.java
@@ -2,13 +2,38 @@ package br.edu.insper.desagil.joice.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraCFCTest {
+	private static double DELTA = 0.05;
+	private CalculadoraCFC calculadora1;
+
+	@BeforeEach
+	private void setUp() {
+		calculadora1 = new CalculadoraCFC();
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testaDensidadeCobre() {
+		double peso = 63.55;
+		double raio = 0.128;
+		assertEquals(8.89, calculadora1.calcula(peso, raio), DELTA);
+	}
+	
+	@Test
+	public void testaDensidadeOuro() {
+		double peso = 196.97;
+		double raio = 0.144;
+		assertEquals(19.36, calculadora1.calcula(peso, raio), DELTA);
+	}
+	
+	@Test
+	public void testaDensidadePrata() {
+		double peso = 107.87;
+		double raio = 0.144;
+		assertEquals(10.60, calculadora1.calcula(peso, raio), DELTA);
 	}
 
 }
+
